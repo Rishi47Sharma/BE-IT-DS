90d811f6d4db630fd942b3bbc9ce164123ac82ba
Rishikesh Sharma <90702705+Rishi47Sharma@users.noreply.github.com>
2024-04-29 20:47:59 +0530

    Removed args from AddClient.java

--- Assign1/AddClient.java
+++ Assign1/AddClient.java
@@ -1,17 +1,24 @@
 import java.rmi.*;
+import java.util.Scanner;
+
 public class AddClient {
-public static void main(String args[]) { 
-try {
-// Get reference to the remote object
-String addServerURL = "rmi://" + args[0] + "/AddServer"; 
-AddServerIntf addServerIntf =
-(AddServerIntf) Naming.lookup(addServerURL);
-System.out.println("The first number is: " + args[1]); 
-double d1 = Double.parseDouble(args[1]);
-System.out.println("The second number is: " + args[2]);
-double d2 = Double.parseDouble(args[2]);
-// Invoke remote method to add numbers
-System.out.println("The sum is: " + addServerIntf.add(d1, d2));
-} 
-catch (Exception e) { System.out.println("Exception: "+ e);
-}}}
+    public static void main(String args[]) {
+        try {
+            // Get reference to the remote object
+            String addServerURL = "rmi://localhost/AddServer";
+            AddServerIntf addServerIntf = (AddServerIntf) Naming.lookup(addServerURL);
+
+            // Read input from the user
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the first number: ");
+            double d1 = scanner.nextDouble();
+            System.out.print("Enter the second number: ");
+            double d2 = scanner.nextDouble();
+
+            // Invoke remote method to add numbers
+            System.out.println("The sum is: " + addServerIntf.add(d1, d2));
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+    }
+}
